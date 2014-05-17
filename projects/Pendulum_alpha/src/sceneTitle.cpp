ac45e5b9caf87a69fbabb8abfc5c96c5f8d0d810
jg-maon <maomona.0513@gmail.com>
2014-05-17 14:09:31 +0900

    タイトル画面処理見本適当に追加

--- projects/Pendulum_alpha/src/sceneTitle.cpp
+++ projects/Pendulum_alpha/src/sceneTitle.cpp
@@ -17,11 +17,65 @@ CSceneTitle::~CSceneTitle()
 {
 }
 
+void CSceneTitle::TitleInit()
+{
+	phaseTime_ = 0.f;
+}
 
+
+void CSceneTitle::TitleStep()
+{
+	phaseTime_ += system::FrameTime;
+	const float demoTime = 2.f;	// デモプレイに移動するまでの時間
+	if (phaseTime_ >= demoTime)
+	{
+		DemoInit();
+		phase_ = Phase::DEMO;	// デモプレイ状態に
+	}
+}
+
+void CSceneTitle::DemoInit()
+{
+	phaseTime_ = 0.f;
+	demo_.pos.x = 100.f;
+}
+void CSceneTitle::DemoStep()
+{
+	demo_.pos.x += 100.f * system::FrameTime;
+	// デモプレイ終了
+	if (demo_.pos.x >= 200.f)
+	{
+		TitleInit();
+		phase_ = Phase::TITLE;	// タイトル画面へ
+	}
+}
 
 // 処理
 IScene* CSceneTitle::step()
 {
+	switch (state_)
+	{
+	case IScene::State::INNING:
+		break;
+	case IScene::State::MAIN:
+		switch (phase_)
+		{
+		case Phase::TITLE:
+			// タイトル画面表示中
+			TitleStep();
+			break;
+		case Phase::DEMO:
+			// デモプレイ表示中
+			DemoStep();
+			break;
+		}
+		break;
+	case IScene::State::OUTING:
+		break;
+	default:
+		break;
+	}
+
 	// 何かアクションを起こしてシーンが切り替わるとき
 	if(input::CheckPush(input::KEY_BTN0))
 	{
--- projects/Pendulum_alpha/src/sceneTitle.h
+++ projects/Pendulum_alpha/src/sceneTitle.h
@@ -7,6 +7,45 @@
 
 class CSceneTitle : public IScene
 {
+private:
+	enum class Phase
+	{
+		TITLE,			// メインタイトル画面
+		DEMO,			// デモプレイ
+	}phase_;			// フェーズ
+
+	float phaseTime_;	// 各フェーズ表示時間
+
+	//-------------------------------------
+	// デモプレイで必要な変数等
+	class CDemo
+	{
+	public:
+		mymath::Vec3f pos;
+	}demo_;
+	//-------------------------------------
+
+private:
+	/*
+		@brief	タイトル画面表示初期化処理
+		@return	なし
+	*/
+	void TitleInit();
+	/*
+		@brief	タイトル画面時の更新処理
+		@return	なし
+	*/
+	void TitleStep();
+	/*
+		@brief	デモプレイ初期化処理
+		@return なし
+	*/
+	void DemoInit();
+	/*
+		@brief	デモプレイ表示中の更新処理
+		@return	なし
+	*/
+	void DemoStep();
 public:
 	CSceneTitle();
 	~CSceneTitle();
